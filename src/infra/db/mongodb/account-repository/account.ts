23964ca8245703a2a3d97d789843237af1707ec3
Ilmar <ilmardanilo@hotmail.com>
2022-12-02 22:06:50 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -5,7 +5,7 @@ import { IAccountModel } from '../../../../domain/models/account';
 
 export class AccountMongoRepository implements IAddAccountRepository {
   async add(accountData: IAddAccountModel): Promise<IAccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts');
+    const accountCollection = await MongoHelper.getCollection('accounts');
 
     const { insertedId } = await accountCollection.insertOne(accountData);
 
