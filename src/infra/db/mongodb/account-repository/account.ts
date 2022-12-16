31e85d51641927fe47e9a11c6d1be816f200f47f
Ilmar <ilmardanilo@hotmail.com>
2022-12-15 21:46:20 -0300

    feat: ensure MongoAccountRepository return an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,13 @@
+import { MongoClient } from 'mongodb';
 import { MongoHelper } from './../helpers/mongo-helper';
 import { IAddAccountRepository } from '../../../../data/protocols/db/add-account-repository';
 import { IAddAccountModel } from '../../../../domain/usecases/add-account';
 import { IAccountModel } from '../../../../domain/models/account';
+import { ILoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository';
 
-export class AccountMongoRepository implements IAddAccountRepository {
+export class AccountMongoRepository
+  implements IAddAccountRepository, ILoadAccountByEmailRepository
+{
   async add(accountData: IAddAccountModel): Promise<IAccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
 
@@ -15,6 +19,19 @@ export class AccountMongoRepository implements IAddAccountRepository {
       email: accountData.email,
       password: accountData.password,
     };
+
+    return account;
+  }
+
+  async loadByEmail(email: string): Promise<any> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const accountResult = await accountCollection.findOne({ email });
+    const account = {
+      id: accountResult._id.toString(),
+      ...accountResult,
+    };
+
+    delete account._id;
 
     return account;
   }
