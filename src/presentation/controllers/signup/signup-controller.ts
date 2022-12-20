1579619cc47d2ac9e0e307bb8874ff58e2a8de59
Ilmar <ilmardanilo@hotmail.com>
2022-12-20 19:56:19 -0300

    feat: ensure SignUpController returns 200 returns an accessToken on success

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -30,12 +30,12 @@ export class SignUpController implements IController {
         password,
       });
 
-      await this.authentication.auth({
+      const accessToken = await this.authentication.auth({
         email,
         password,
       });
 
-      return ok(account);
+      return ok({ accessToken });
     } catch (error) {
       return serverError(error);
     }
