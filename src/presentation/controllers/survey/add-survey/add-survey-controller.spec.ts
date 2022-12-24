ae253be2e947221c6090808e97cf2ec5f179723c
Ilmar <ilmardanilo@hotmail.com>
2022-12-24 12:15:40 -0300

    test: ensure AddSurveyController calls Validation with correct values

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -0,0 +1,47 @@
+import { AddSurveyController } from './add-survey-controller';
+import { IHttpRequest, IValidation } from './add-survey-controller-protocols';
+
+const makeFakeRequest = (): IHttpRequest => ({
+  body: {
+    question: 'any_question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer',
+      },
+    ],
+  },
+});
+
+const makeValidation = (): IValidation => {
+  class ValidationStub implements IValidation {
+    validate(input: any): Error {
+      return null;
+    }
+  }
+
+  return new ValidationStub();
+};
+
+interface ISutTypes {
+  sut: AddSurveyController;
+  validationStub: IValidation;
+}
+
+const makeSut = (): ISutTypes => {
+  const validationStub = makeValidation();
+  const sut = new AddSurveyController(validationStub);
+  return {
+    sut,
+    validationStub,
+  };
+};
+describe('AddSurvey Controller', () => {
+  test('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut();
+    const validateSpy = jest.spyOn(validationStub, 'validate');
+    const httpRequest = makeFakeRequest();
+    await sut.handle(httpRequest);
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+});
