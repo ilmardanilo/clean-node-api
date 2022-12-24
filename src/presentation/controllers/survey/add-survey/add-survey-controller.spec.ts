98658d8b61b64be37867645c9314851fbe6cbdac
Ilmar <ilmardanilo@hotmail.com>
2022-12-24 12:20:20 -0300

    test: ensure AddSurveyController returns 400 if Validation fails

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,3 +1,5 @@
+import { MissingParamError } from '../../../errors';
+import { badRequest } from '../../../helpers/http/http-helper';
 import { AddSurveyController } from './add-survey-controller';
 import { IHttpRequest, IValidation } from './add-survey-controller-protocols';
 
@@ -43,5 +45,16 @@ describe('AddSurvey Controller', () => {
     const httpRequest = makeFakeRequest();
     await sut.handle(httpRequest);
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  test('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut();
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new MissingParamError('any_field'));
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(
+      badRequest(new MissingParamError('any_field'))
+    );
   });
 });
