a087f392e4b2a2b6a7d14daf6336e47a368859d7
Ilmar <ilmardanilo@hotmail.com>
2022-12-25 22:35:20 -0300

    test: ensure add survey route returns 204 on add survey with valid accessToken

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,13 @@
+import { sign } from 'jsonwebtoken';
 import request from 'supertest';
 import app from '../config/app';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import { Collection } from 'mongodb';
+import { hash } from 'bcrypt';
+import env from '../config/env';
 
 let surveyCollection: Collection;
+let accountCollection: Collection;
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -17,6 +21,8 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys');
     await surveyCollection.deleteMany({});
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
   });
 
   describe('POST /surveys', () => {
@@ -36,6 +42,42 @@ describe('Survey Routes', () => {
           ],
         })
         .expect(403);
+    });
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const password = await hash('123', 12);
+      const { insertedId } = await accountCollection.insertOne({
+        name: 'Ilmar',
+        email: 'ilmar@mail.com',
+        password,
+        role: 'admin',
+      });
+      const id = insertedId.toString();
+      const accessToken = sign(id, env.jwtSecret);
+      await accountCollection.updateOne(
+        { _id: insertedId },
+        {
+          $set: {
+            accessToken,
+          },
+        }
+      );
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              answer: 'Answer 1',
+              image: 'http://image-name.com',
+            },
+            {
+              answer: 'Answer 2',
+            },
+          ],
+        })
+        .expect(204);
     });
   });
 });
