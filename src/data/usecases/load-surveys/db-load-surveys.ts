5717d02980d29d7ecd2643d8fc4f145cfe17209c
Ilmar <ilmardanilo@hotmail.com>
2022-12-26 22:30:10 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,11 @@
+import { ILoadSurveys } from './../../../domain/usecases/load-surveys';
+import { ISurveyModel } from '../../../domain/models/survey';
+import { ILoadSurveysRepository } from './../../protocols/db/survey/load-surveys-repository';
+
+export class DbLoadSurveys implements ILoadSurveys {
+  constructor(private readonly loadSurveysRepository: ILoadSurveysRepository) {}
+  async load(): Promise<ISurveyModel[]> {
+    await this.loadSurveysRepository.loadAll();
+    return [];
+  }
+}
