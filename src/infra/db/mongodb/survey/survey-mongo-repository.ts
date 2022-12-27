d99d2bd772115f2a09185082ddca6dc1832628cb
Ilmar <ilmardanilo@hotmail.com>
2022-12-27 17:17:43 -0300

    feat: ensure SurveyMongoRepository load all surveys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,11 +1,23 @@
+import { ILoadSurveysRepository } from './../../../../data/protocols/db/survey/load-surveys-repository';
 import { IAddSurveyRepository } from './../../../../data/protocols/db/survey/add-survey-repository';
 import { IAddSurveyModel } from '../../../../domain/usecases/add-survey';
 import { MongoHelper } from '../helpers/mongo-helper';
+import { ISurveyModel } from '../../../../domain/models/survey';
 
-export class SurveyMongoRepository implements IAddSurveyRepository {
+export class SurveyMongoRepository
+  implements IAddSurveyRepository, ILoadSurveysRepository
+{
   async add(surveyData: IAddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys');
 
     await surveyCollection.insertOne(surveyData);
+  }
+
+  async loadAll(): Promise<ISurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys');
+
+    const surveys: any = await surveyCollection.find().toArray();
+
+    return surveys;
   }
 }
