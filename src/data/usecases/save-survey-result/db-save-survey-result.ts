22630aba361e45be0acb932548a26ffc4621d88f
Ilmar <ilmardanilo@hotmail.com>
2022-12-29 11:34:39 -0300

    feat: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/save-survey-result/db-save-survey-result.ts
@@ -0,0 +1,17 @@
+import {
+  SurveyResultModel,
+  ISaveSurveyResultRepository,
+  ISaveSurveyResult,
+  SaveSurveyResultModel,
+} from './db-save-survey-result-protocols';
+
+export class DbSaveSurveyResult implements ISaveSurveyResult {
+  constructor(
+    private readonly saveSurveyResultRepository: ISaveSurveyResultRepository
+  ) {}
+
+  async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    await this.saveSurveyResultRepository.save(data);
+    return null;
+  }
+}
