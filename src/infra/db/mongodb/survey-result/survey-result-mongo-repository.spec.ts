33072aeb9e0933ab99be1e271f8afd3de2bb905d
Ilmar <ilmardanilo@hotmail.com>
2022-12-30 20:02:25 -0300

    test: save a survey result if its new

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -0,0 +1,81 @@
+import { Collection } from 'mongodb';
+import { SurveyModel } from '../../../../domain/models/survey';
+import { AccountModel } from '../../../../domain/models/account';
+import { MongoHelper } from '../helpers/mongo-helper';
+import { SurveyResultMongoRepository } from './survey-result-mongo-repository';
+
+let surveyCollection: Collection;
+let surveyResultCollection: Collection;
+let accountCollection: Collection;
+
+const makeSut = (): SurveyResultMongoRepository => {
+  return new SurveyResultMongoRepository();
+};
+
+const makeSurvey = async (): Promise<SurveyModel> => {
+  const { insertedId } = await surveyCollection.insertOne({
+    question: 'any_question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer',
+      },
+      {
+        answer: 'other_answer',
+      },
+    ],
+    date: new Date(),
+  });
+
+  const survey: any = await surveyCollection.findOne({ _id: insertedId });
+
+  return MongoHelper.map(survey);
+};
+
+const makeAccountId = async (): Promise<string> => {
+  const { insertedId } = await accountCollection.insertOne({
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password',
+  });
+
+  return insertedId.toString();
+};
+
+describe('Survey Result Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    await surveyCollection.deleteMany({});
+
+    surveyResultCollection = await MongoHelper.getCollection('surveyResults');
+    await surveyResultCollection.deleteMany({});
+
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
+  describe('save()', () => {
+    test('Should save a survey result if its new', async () => {
+      const survey = await makeSurvey();
+      const accountId = await makeAccountId();
+      const sut = makeSut();
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId,
+        answer: survey.answers[0].answer,
+        date: new Date(),
+      });
+      expect(surveyResult).toBeTruthy();
+      expect(surveyResult.id).toBeTruthy();
+      expect(surveyResult.answer).toBe(survey.answers[0].answer);
+    });
+  });
+});
