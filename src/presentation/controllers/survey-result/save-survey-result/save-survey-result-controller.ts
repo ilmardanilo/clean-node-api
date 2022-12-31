7f394f9b6498e3770118e350a7ff68ea5d47b84a
Ilmar <ilmardanilo@hotmail.com>
2022-12-31 15:58:29 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,15 +1,20 @@
+import { ISaveSurveyResult } from '../../../../domain/usecases/survey-result/save-survey-result';
 import { ILoadSurveyById } from '../../../../domain/usecases/survey/load-survey-by-id';
 import { InvalidParamError } from '../../../errors';
 import { forbidden, serverError } from '../../../helpers/http/http-helper';
 import { HttpRequest, HttpResponse, IController } from '../../../protocols';
 
 export class SaveSurveyResultController implements IController {
-  constructor(private readonly loadSurveyById: ILoadSurveyById) {}
+  constructor(
+    private readonly loadSurveyById: ILoadSurveyById,
+    private readonly saveSurveyResult: ISaveSurveyResult
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params;
       const { answer } = httpRequest.body;
+      const { accountId } = httpRequest;
 
       const survey = await this.loadSurveyById.loadById(surveyId);
 
@@ -21,6 +26,14 @@ export class SaveSurveyResultController implements IController {
       } else {
         return forbidden(new InvalidParamError('surveyId'));
       }
+
+      await this.saveSurveyResult.save({
+        surveyId,
+        accountId,
+        answer,
+        date: new Date(),
+      });
+
       return null;
     } catch (error) {
       return serverError(error);
