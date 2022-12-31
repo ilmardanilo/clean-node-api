b48a98693cfbaf3ecc01b6eca212e29cad0cd86d
Ilmar <ilmardanilo@hotmail.com>
2022-12-31 19:14:47 -0300

    test: ensure save survey result route return 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,6 +1,34 @@
+import { sign } from 'jsonwebtoken';
 import request from 'supertest';
 import app from '../config/app';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
+import { Collection } from 'mongodb';
+import { hash } from 'bcrypt';
+import env from '../config/env';
+
+let surveyCollection: Collection;
+let accountCollection: Collection;
+
+const makeAccessToken = async (): Promise<string> => {
+  const password = await hash('123', 12);
+  const { insertedId } = await accountCollection.insertOne({
+    name: 'Ilmar',
+    email: 'ilmar@mail.com',
+    password,
+  });
+  const id = insertedId.toString();
+  const accessToken = sign(id, env.jwtSecret);
+  await accountCollection.updateOne(
+    { _id: insertedId },
+    {
+      $set: {
+        accessToken,
+      },
+    }
+  );
+
+  return accessToken;
+};
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -11,12 +39,41 @@ describe('Survey Routes', () => {
     await MongoHelper.disconnect();
   });
 
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    await surveyCollection.deleteMany({});
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
   describe('PUT /surveys/:survey_id/results', () => {
     test('Should return 403 on save survey result without accessToken', async () => {
       await request(app)
         .put('/api/surveys/any_id/results')
         .send({ answer: 'any_answer' })
         .expect(403);
+    });
+
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const { insertedId } = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'Answer 1',
+          },
+          {
+            answer: 'other_answer',
+          },
+        ],
+        date: new Date(),
+      });
+      const accessToken = await makeAccessToken();
+      await request(app)
+        .put(`/api/surveys/${insertedId}/results`)
+        .set('x-access-token', accessToken)
+        .send({ answer: 'Answer 1' })
+        .expect(200);
     });
   });
 });
