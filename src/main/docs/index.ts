4780b2673c64cc8c97694b5f59c307f0104f2cc9
ilmar <ilmar.danilo@querodelivery.com>
2023-01-02 12:24:54 -0300

    docs: add api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,9 +1,45 @@
+import paths from './paths';
+import components from './components';
+import schemas from './schemas';
+
 export default {
   openapi: '3.0.0',
   info: {
-    title: 'Clean Node API',
+    title: '4Dev - Enquetes para Programadores',
     description:
-      'API do curso do Mango para realizar enquetes entre programadores',
+      'Essa é a documentação da API feita pelo instrutor Rodrigo Manguinho no curso da Udemy de NodeJs usando Typescript, TDD, Clean Architecture e seguindo os princípios do SOLID e Design Patterns.',
     version: '1.0.0',
+    contact: {
+      name: 'Ilmar Danilo',
+      email: 'ilmardanilosl@gmail.com',
+      url: 'https://www.linkedin.com/in/ilmardanilo',
+    },
+    license: {
+      name: 'GPL-3.0-or-later',
+      url: 'https://spdx.org/licenses/GPL-3.0-or-later.html',
+    },
   },
+  externalDocs: {
+    description: 'Link para o treinamento completo',
+    url: 'https://www.udemy.com/course/tdd-com-mango/?referralCode=B53CE5CA2B9AFA5A6FA1',
+  },
+  servers: [
+    {
+      url: '/api',
+      description: 'Servidor Principal',
+    },
+  ],
+  tags: [
+    {
+      name: 'Login',
+      description: 'APIs relacionadas a Login',
+    },
+    {
+      name: 'Enquete',
+      description: 'APIs relacionadas a Enquete',
+    },
+  ],
+  paths,
+  schemas,
+  components,
 };
