7f8d7b3d7d4040628138cbaa2e482cc276fdbfeb
ilmar <ilmar.danilo@querodelivery.com>
2023-01-03 11:02:05 -0300

    test: ensure DbLoadSurveyResult returns surveyResultModel on success

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -1,7 +1,7 @@
 import { DbLoadSurveyResult } from './db-load-survey-result'
 import { LoadSurveyResultRepository } from './db-load-survey-result-protocols'
 import { mockLoadSurveyResultRepository } from '@/data/test'
-import { throwError } from '@/domain/test'
+import { mockSurveyResultModel, throwError } from '@/domain/test'
 
 type SutTypes = {
   sut: DbLoadSurveyResult
@@ -36,5 +36,11 @@ describe('DbLoadSurveyResult UseCase', () => {
       .mockImplementationOnce(throwError)
     const promise = sut.load('any_survey_id')
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return surveyResultModel on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.load('any_survey_id')
+    expect(httpResponse).toEqual(mockSurveyResultModel())
   })
 })
