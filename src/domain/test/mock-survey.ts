ff4017fe3eaa607dccb5ef1dadfa277ecb0a618b
ilmar <ilmar.danilo@querodelivery.com>
2023-01-03 14:10:06 -0300

    feat: ensure DbLoadSurveyResult returns empty survey result if there is no result

--- src/domain/test/mock-survey.ts
+++ src/domain/test/mock-survey.ts
@@ -5,39 +5,53 @@ export const mockSurveyModel = (): SurveyModel => {
   return {
     id: 'any_id',
     question: 'any_question',
-    answers: [{
-      image: 'any_image',
-      answer: 'any_answer'
-    }],
+    answers: [
+      {
+        answer: 'any_answer'
+      },
+      {
+        answer: 'other_answer',
+        image: 'any_image'
+      }
+    ],
     date: new Date()
   }
 }
 
 export const mockSurveyModels = (): SurveyModel[] => {
-  return [{
-    id: 'any_id',
-    question: 'any_question',
-    answers: [{
-      image: 'any_image',
-      answer: 'any_answer'
-    }],
-    date: new Date()
-  }, {
-    id: 'other_id',
-    question: 'other_question',
-    answers: [{
-      image: 'other_image',
-      answer: 'other_answer'
-    }],
-    date: new Date()
-  }]
+  return [
+    {
+      id: 'any_id',
+      question: 'any_question',
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer'
+        }
+      ],
+      date: new Date()
+    },
+    {
+      id: 'other_id',
+      question: 'other_question',
+      answers: [
+        {
+          image: 'other_image',
+          answer: 'other_answer'
+        }
+      ],
+      date: new Date()
+    }
+  ]
 }
 
 export const mockAddSurveyParams = (): AddSurveyParams => ({
   question: 'any_question',
-  answers: [{
-    image: 'any_image',
-    answer: 'any_answer'
-  }],
+  answers: [
+    {
+      image: 'any_image',
+      answer: 'any_answer'
+    }
+  ],
   date: new Date()
 })
