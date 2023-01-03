234fdcf3c53acb80a3ce29e8f680b1a79289ce8b
ilmar <ilmar.danilo@querodelivery.com>
2023-01-03 10:27:34 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct value

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,5 @@ export default {
     process.env.MONGO_URL ||
     'mongodb+srv://mongodb:mongodb123@sandbox.hrm79pu.mongodb.net/?retryWrites=true&w=majority',
   port: process.env.PORT || 3333,
-  jwtSecret: process.env.JWT_SECRET || 'A@lt10_$14',
-};
+  jwtSecret: process.env.JWT_SECRET || 'A@lt10_$14'
+}
