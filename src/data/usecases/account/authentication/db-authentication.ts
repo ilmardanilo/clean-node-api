bc4ef560f6dccd500de4f939832887f00e4912fb
ilmar <ilmar.danilo@querodelivery.com>
2023-01-05 12:40:28 -0300

    feat: add name to authentication response

--- src/data/usecases/account/authentication/db-authentication.ts
+++ src/data/usecases/account/authentication/db-authentication.ts
@@ -1,4 +1,12 @@
-import { Authentication, AuthenticationParams, LoadAccountByEmailRepository, HashComparer, Encrypter, UpdateAccessTokenRepository } from './db-authentication-protocols'
+import {
+  Authentication,
+  AuthenticationParams,
+  LoadAccountByEmailRepository,
+  HashComparer,
+  Encrypter,
+  UpdateAccessTokenRepository,
+  AuthenticationModel
+} from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   constructor (
@@ -8,14 +16,24 @@ export class DbAuthentication implements Authentication {
     private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
-  async auth (authentication: AuthenticationParams): Promise<string> {
-    const account = await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
+  async auth (
+    authentication: AuthenticationParams
+  ): Promise<AuthenticationModel> {
+    const account = await this.loadAccountByEmailRepository.loadByEmail(
+      authentication.email
+    )
     if (account) {
-      const isValid = await this.hashComparer.compare(authentication.password, account.password)
+      const isValid = await this.hashComparer.compare(
+        authentication.password,
+        account.password
+      )
       if (isValid) {
         const accessToken = await this.encrypter.encrypt(account.id)
-        await this.updateAccessTokenRepository.updateAccessToken(account.id, accessToken)
-        return accessToken
+        await this.updateAccessTokenRepository.updateAccessToken(
+          account.id,
+          accessToken
+        )
+        return { accessToken, name: account.name }
       }
     }
     return null
